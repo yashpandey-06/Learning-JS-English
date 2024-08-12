8bdb8e513e7e11ff3433b0fcf16ce1733017a872
yashpandey-06 <yashpandey31790@gmail.com>
2024-08-12 17:26:03 +0000

    Push Config Changes

---
+++ 01_Objects/01_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"
+//treat all JS code as newer version
+
+//alert(3+3) we are using nodejs,not browser//
+
+console.log(3+3); 
+console.log("YASH")
+//code readability should be high//
+//ecma pai sbb pdhlena agr jarurat pde vrna mdn krlo//
+
+
+let name = "YASH"
+let age = 18
+let isloggedIn = false
+
+// number =>range 2^53//
+// bigint  tbhi use hoga jbb number bhot bhot hi jyada bdi ho//
+// string =>"" hmesa inne mai hi define hoti hai
+// boolean => true/false
+// null =>it is a standalone value hai
+// undefined =>means value define nhi ki hai
+// symbol => jbb  unique ki baat krni ho tbb likh dete ahi
+
+console.log(typeof "YASH")
+console.log(typeof undefined)
+console.log(typeof null)
+
+// object
